f076d1c7ecfbbdb5ce7619a37234e77fa497f844
Joe Frikker <jfrikker@gmail.com>
2018-02-09 22:40:52 -0500

    Better error handling

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate clap;
 extern crate time;
 
 use std::ffi::{OsStr, OsString};
-use std::io::{Write, stdout};
+use std::io::{Error, Write, stdout, stderr};
 use std::os::unix::ffi::{OsStrExt, OsStringExt};
 use std::process;
 use std::time::{Duration, Instant};
@@ -12,10 +12,9 @@ use std::thread::sleep;
 
 fn main() {
     let matches = 
-        clap_app!(myapp =>
+        clap_app!(poll =>
             (version: "1.0")
-            (author: "Joe Frikker")
-            (about: "polls")
+            (about: "Runs a command repeatedly on some interval. Watches for changes to the output, and reacts to them.")
             (@setting TrailingVarArg)
             (@arg TIMESTAMP: -t --timestamp "Print timestamps")
             (@arg EXIT_CODE: -x --exit_code "Poll exit code, not stdout")
@@ -23,10 +22,21 @@ fn main() {
             (@arg UNTIL_FAILURE: -f --until_failure requires[EXIT_CODE] conflicts_with[UNTIL_SUCCESS] "Exit on failure")
             (@arg SHELL: -s --shell "Expect a single argument, which will be run in a shell")
             (@arg QUIET: -q --quiet "Suppress output")
-            (@arg INTERVAL: -i --interval +takes_value "Polling interval, in seconds")
+            (@arg INTERVAL: -i --interval +takes_value "Polling interval, in seconds (default 1 second)")
             (@arg CMD: ... * "Command to run")
         ).get_matches();
+    
+    match do_loop(&matches) {
+        Ok(x) => x,
+        Err(err) => {
+            let mut stderr = stderr();
+            write!(&mut stderr, "{}\n", err.to_string());
+            process::exit(1);
+        }
+    }
+}
 
+fn do_loop(matches: &clap::ArgMatches) -> Result<(), Error> {
     let print_timestamp = matches.is_present("TIMESTAMP");
     let use_code = matches.is_present("EXIT_CODE");
     let until_success = matches.is_present("UNTIL_SUCCESS");
@@ -49,24 +59,24 @@ fn main() {
     loop {
         timer.wait();
         let cmd_result = if use_code {
-            let status = exit_code(&args);
+            let status = try!(exit_code(&args));
 
             if status.success() {
                 if until_success {
-                    process::exit(0);
+                    return Ok(());
                 }
 
                 OsString::from("Success\n")
             } else {
                 if until_failure {
-                    process::exit(0);
+                    return Ok(());
                 }
 
                 OsString::from(format!("Failed ({})\n", status.code()
                     .unwrap_or(1000)))
             }
         } else {
-            output(&args)
+            output(&args).unwrap()
         };
 
         if cmd_result == last {
@@ -79,8 +89,8 @@ fn main() {
                 print!("{} - ", timestamp);
             }
 
-            stdout().write(cmd_result.as_os_str().as_bytes()).unwrap();
-            stdout().flush().unwrap();
+            try!(stdout().write(cmd_result.as_os_str().as_bytes()));
+            try!(stdout().flush());
         }
 
         last = cmd_result
@@ -121,21 +131,18 @@ fn to_millis(d: &Duration) -> u64 {
     (d.as_secs() * 1000) + (d.subsec_nanos() / 1000000) as u64
 }
 
-fn exit_code(cmd: &Vec<&OsStr>) -> process::ExitStatus {
+fn exit_code(cmd: &Vec<&OsStr>) -> Result<process::ExitStatus, Error> {
     process::Command::new(&cmd[0])
         .args(&cmd[1..])
         .stdout(process::Stdio::null())
         .stderr(process::Stdio::null())
         .status()
-        .unwrap()
 }
 
-fn output(cmd: &Vec<&OsStr>) -> OsString {
-    let out = process::Command::new(&cmd[0])
+fn output(cmd: &Vec<&OsStr>) -> Result<OsString, Error> {
+    process::Command::new(&cmd[0])
         .args(&cmd[1..])
         .stderr(process::Stdio::null())
         .output()
-        .unwrap()
-        .stdout;
-    OsString::from_vec(out)
+        .map(|res| OsString::from_vec(res.stdout))
 }
