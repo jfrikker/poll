a3ec7f3244ba20b1b003efba768bf846386b01c4
Joe Frikker <jfrikker@gmail.com>
2018-02-09 20:26:01 -0500

    -i and -t

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,70 @@
-use std::env;
+#[macro_use]
+extern crate clap;
+
+extern crate time;
+
+use std::ffi::OsStr;
 use std::process;
 use std::time::{Duration, Instant};
 use std::thread::sleep;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let matches = 
+        clap_app!(myapp =>
+            (version: "1.0")
+            (author: "Joe Frikker")
+            (about: "polls")
+            (@setting TrailingVarArg)
+            (@arg TIMESTAMP: -t --timestamp "Print timestamps")
+            (@arg INTERVAL: -i --interval +takes_value "Polling interval, in seconds")
+            (@arg CMD: ... * "Command to run")
+        ).get_matches();
 
-    let mut stream = diffing(with_interval(1000, cmd_exit_code(args[1..].to_vec())));
+    let args: Vec<&OsStr> = matches.values_of_os("CMD").unwrap().collect();
+
+    let interval_sec: u64 = matches.value_of("INTERVAL")
+        .map_or(1, |s| s.parse().unwrap());
+
+    let print_timestamp = matches.is_present("TIMESTAMP");
+
+    let mut timer = Timer::new(interval_sec * 1000);
+    let mut last = String::from("");
 
     loop {
-        print!("{}\n", stream.next());
-    }
-}
-
-trait CmdStream<T> {
-    fn next(&mut self) -> T;
-}
-
-struct CmdExitCode {
-    args: Vec<String>
-}
-
-fn cmd_exit_code(args: Vec<String>) -> CmdExitCode {
-    CmdExitCode {
-        args : args
-    }
-}
-
-impl CmdStream<String> for CmdExitCode {
-    fn next(&mut self) -> String {
-        let exit = process::Command::new(&self.args[0])
-            .args(&self.args[1..])
-            .stdout(process::Stdio::null())
-            .stderr(process::Stdio::null())
-            .status()
-            .unwrap();
-
-        if exit.success() {
-            String::from("Success")
-        } else {
-            format!("Failed ({})", exit.code()
-                .unwrap_or(1000))
+        timer.wait();
+        let cmd_result = exit_code(&args);
+        if cmd_result == last {
+            continue;
         }
+
+        if print_timestamp {
+            let timestamp = time::strftime("%F %H:%M:%S", &time::now()).unwrap();
+            print!("{} - ", timestamp);
+        }
+
+        print!("{}\n", cmd_result);
+        last = cmd_result
     }
 }
 
-struct Timer<S> {
-    underlying: S,
+struct Timer {
     interval_ms: u64,
     last: Option<Instant>
 }
 
-fn with_interval<T, S: CmdStream<T>>(interval_ms: u64, underlying: S) -> Timer<S> {
-    Timer {
-        underlying: underlying,
-        interval_ms: interval_ms,
-        last: None
+impl Timer {
+    fn new(interval_ms: u64) -> Timer {
+        Timer {
+            interval_ms: interval_ms,
+            last: None
+        }
     }
-}
 
-impl <T, S: CmdStream<T>> CmdStream<T> for Timer<S> {
-    fn next(&mut self) -> T {
+    fn wait(&mut self) {
         let now = Instant::now();
         self.last = Some(match self.last {
             Some(l) => {
-                let elapsed = to_millis(now.duration_since(l));
+                let elapsed = to_millis(&now.duration_since(l));
                 if elapsed < self.interval_ms {
                     sleep(Duration::from_millis(self.interval_ms - elapsed));
                     Instant::now()
@@ -74,44 +74,25 @@ impl <T, S: CmdStream<T>> CmdStream<T> for Timer<S> {
             },
             None => now
         });
-
-        self.underlying.next()
     }
 }
 
-fn to_millis(d: Duration) -> u64 {
+fn to_millis(d: &Duration) -> u64 {
     (d.as_secs() * 1000) + (d.subsec_nanos() / 1000000) as u64
 }
 
-struct Diff<S, T> {
-    underlying: S,
-    last: Option<T>
-}
+fn exit_code(cmd: &Vec<&OsStr>) -> String {
+    let exit = process::Command::new(&cmd[0])
+        .args(&cmd[1..])
+        .stdout(process::Stdio::null())
+        .stderr(process::Stdio::null())
+        .status()
+        .unwrap();
 
-fn diffing<T, S: CmdStream<T>>(underlying: S) -> Diff<S, T> {
-    Diff {
-        underlying: underlying,
-        last: None
-    }
-}
-
-impl <T: Eq + Clone + std::fmt::Display, S: CmdStream<T>> CmdStream<T> for Diff<S, T> {
-    fn next(&mut self) -> T {
-        let mut done = false;
-        while !done {
-            let next = self.underlying.next();
-            match self.last {
-                Some(ref l) => {
-                    if next != *l {
-                        done = true;
-                    }
-                },
-                None => {}
-            }
-
-            self.last = Some(next);
-        }
-
-        self.last.as_ref().unwrap().clone()
+    if exit.success() {
+        String::from("Success")
+    } else {
+        format!("Failed ({})", exit.code()
+            .unwrap_or(1000))
     }
 }
